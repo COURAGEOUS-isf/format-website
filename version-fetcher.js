c2d6566fe2803636cd8a4aa2dc8fbcddb224f818
aleokdev <aleok.inf@gmail.com>
2023-06-15 14:25:46 +0200

    Finish main pass

--- version-fetcher.js
+++ version-fetcher.js
@@ -6,9 +6,12 @@ function onLoad() {
             for (const version of json) {
                 content += `<div>
                     <a class="version-link button" target="_blank" href="schemas/schema-${version.name}.json"><img type="image/svg+xml" src="download-solid.svg"></img> Download</a>
-                    <a class="version-link button" target="_blank" href="https://grvc.us.es/courageous/visualizer/?hideEditor&maxLevel=99&surl=/courageous/schema-${version.name}.json"><img type="image/svg+xml" src="eye-solid.svg"></img> Preview</a>
-                    ${version.name}
-                </div>`;
+                    <a class="version-link button" target="_blank" href="visualizer/?hideEditor&maxLevel=99&surl=/courageous/schemas/${version.name}.json"><img type="image/svg+xml" src="file-lines-solid.svg"></img> Docs</a>
+                    ${version.name}`;
+                if (version.draft) {
+                    content += `<span class="draft-badge">DRAFT</span>`;
+                }
+                content += "</div>";
             }
             document.getElementById("content").innerHTML = content;
         });
