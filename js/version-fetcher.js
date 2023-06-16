f02f43e0e750889f51725aa6a1659bab844ef244
aleokdev <aleok.inf@gmail.com>
2023-06-16 12:17:42 +0200

    Use relative paths on visualizer link

--- js/version-fetcher.js
+++ js/version-fetcher.js
@@ -6,7 +6,7 @@ function onLoad() {
             for (const version of json) {
                 content += `<div>
                     <a class="version-link button" target="_blank" href="schemas/${version.name}.json"><img type="image/svg+xml" src="img/download-solid.svg"></img> Download</a>
-                    <a class="version-link button" target="_blank" href="visualizer/?hideEditor&maxLevel=99&surl=/courageous/schemas/${version.name}.json"><img type="image/svg+xml" src="img/file-lines-solid.svg"></img> Docs</a>
+                    <a class="version-link button" target="_blank" href="visualizer/?hideEditor&maxLevel=99&surl=../schemas/${version.name}.json"><img type="image/svg+xml" src="img/file-lines-solid.svg"></img> Docs</a>
                     ${version.name}`;
                 if (version.draft) {
                     content += `<span class="draft-badge">DRAFT</span>`;
