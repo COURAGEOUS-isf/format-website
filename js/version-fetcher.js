f213eb419e8169315639b6c1ca9b5969dd3495b4
aleokdev <aleok.inf@gmail.com>
2023-07-04 12:51:47 +0200

    Add track2kml downloads section

--- js/version-fetcher.js
+++ js/version-fetcher.js
@@ -53,5 +53,59 @@ function onLoad() {
                 }
             }
         });
+
+    fetch("track2kml/list.json")
+        .then((response) => response.json())
+        .then(async (json) => {
+            let content = "";
+            for (const version of json) {
+                content += `<div>`;
+                content += `<div style="white-space:nowrap;">`;
+                content += `<a class="version-link button" target="_blank" href="track2kml/${version.name}.zip"><img type="image/svg+xml" src="img/download-solid.svg"></img> Download</a>`
+
+                let changelog;
+                try {
+                    const response = await fetch(`track2kml/changelogs/${version.name}.html`);
+
+                    if (response.status === 200) {
+                        changelog = await response.text();
+                    } else {
+                        changelog = null;
+                    }
+                } catch (error) {
+                    console.error(error);
+                    changelog = null;
+                }
+
+                if (changelog !== null) {
+                    content += `<button id="changelog2-${version.name}-button" class="version-link button" href="#"><img type="image/svg+xml" src="img/file-lines-solid.svg"></img> Changelog</button>`
+                }
+                if (version.draft) {
+                    content += `<span class="draft-badge">DRAFT</span>`;
+                }
+                content += version.name;
+                content += "</div>";
+                if (changelog !== null) {
+                    content += `<div id="changelog2-${version.name}" style="display:none;"><h3>Changelog for ${version.name}:</h3>${changelog}</div>`;
+                }
+                content += "</div>";
+            }
+            document.getElementById("content2").innerHTML = content;
+
+            for (const version of json) {
+                let button = document.getElementById(`changelog2-${version.name}-button`);
+                if (button) {
+                    button.addEventListener("click", () => {
+                        console.log(document);
+                        let style = document.getElementById(`changelog2-${version.name}`).style;
+                        if (style.display == "none") {
+                            style.display = "block"
+                        } else {
+                            style.display = "none"
+                        }
+                    }, false);
+                }
+            }
+        });
 }
 window.onload = onLoad;
