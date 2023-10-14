dbf1cfd321ff3b1701471aaf6cb259164dca97b7
aleokdev <aleok.inf@gmail.com>
2023-10-14 18:39:39 +0200

    Add schema v0.4.0 + other tweaks - Minor visual tweaks - Specify track2kml ver with partial/total support for a format version

--- js/version-fetcher.js
+++ js/version-fetcher.js
@@ -24,15 +24,24 @@ function onLoad() {
                 }
 
                 if (changelog !== null) {
-                    content += `<button id="changelog-${version.name}-button" class="version-link button" href="#"><img type="image/svg+xml" src="img/file-lines-solid.svg"></img> Changelog</button>`
+                    content += `<button id="changelog-${version.name}-button" class="version-link button" href="#"><img type="image/svg+xml" src="img/file-lines-solid.svg"></img> Info</button>`
                 }
                 if (version.draft) {
-                    content += `<span class="draft-badge">DRAFT</span>`;
+                    content += `<span class="draft badge">DRAFT</span>`;
                 }
                 content += version.name;
                 content += "</div>";
                 if (changelog !== null) {
-                    content += `<div id="changelog-${version.name}" style="display:none;"><h3>Changelog for ${version.name}:</h3>${changelog}</div>`;
+                    content += `<div id="changelog-${version.name}" style="display:none; padding-left:2em; border-left-color:black;border-left-width: 5px;border-left-style: dotted;"><h3>Changelog for ${version.name}:</h3>${changelog}`;
+                    if (version.track2kml_total) {
+                        content += `<h3 style="font-size:80%;">Track2kml versions with complete support: <span class="badge complete-support">${version.track2kml_total}</span></h3>`;
+                    } else {
+                        content += `<h3 style="font-size:80%;">No version of track2kml exists with complete support for this version</h3>`;
+                    }
+                    if (version.track2kml_partial) {
+                        content += `<h3 style="font-size:80%;">Track2kml versions with partial support: <span class="badge partial-support">${version.track2kml_partial}</span></h3>`;
+                    }
+                    content += `</div>`;
                 }
                 content += "</div>";
             }
